5719fdc7274f9ed8fc631ba851d4b5867dea26f6
nightwing891 <henrydoan96@gmail.com>
2015-09-01 08:59:05 -0600

    First

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Animate-Name-exercise
-Recreated from the code academy course 
+Recreated from the code academy course
+All Rights reserved for the course.
+
+https://www.codecademy.com/forums/animate-your-name/2    - link to the forum and additional content to further personalize the project.
---
+++ main.js
@@ -0,0 +1,19 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Henry Doan";
+letterColors = [red, orange, green, purple, blue]
+if(15>5) {
+    bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+
+
+drawName(myName, letterColors);
+bounceBubbles()
